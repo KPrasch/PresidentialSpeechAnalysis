d78a19ecde52d4ac0877212e2189959cdf5e3c9a
Gabrielle Simard-Moore <gsimardmoore@gmail.com>
2016-11-22 23:00:24 -0800

    Remodeled the presidents, added elections table.

--- presidents/profiles/models.py
+++ presidents/profiles/models.py
@@ -3,13 +3,32 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class President(models.Model):
-    name = models.CharField(max_length=100)
+class Politician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateTimeField()
+    deceased_date = models.DateTimeField(blank=True, null=True)
+    years_lived = models.PositiveSmallIntegerField()
+
+
+class President(Politician):
+    presidecy_number = models.PositiveSmallIntegerField()
+    presidency_start_year = models.DateTimeField()
+    presidency_end_year = models.DateTimeField()
     ari_score = models.PositiveSmallIntegerField(blank=True, null=True)
     # TODO: wordcloud image storage ref?
 
     def __str__(self):
         return self.name
+
+
+class Election(models.Model):
+    count = models.PositiveSmallIntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    candidates = models.ManyToManyField(Politician)
+    president_elect = models.ForeignKey(President)
+    vice_president_elect = models.ForeignKey(Politician, blank=True, null=True)
 
 
 class Speech(models.Model):
